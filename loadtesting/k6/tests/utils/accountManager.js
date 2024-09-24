1bb038d2b3d06238651c99a12e51d2ac178d8f19
romanova-natasha <47331640+romanova-natasha@users.noreply.github.com>
2024-09-24 01:39:48 +0100

    add fundAccount function with an opportunity to choose a source: faucet or bank account or any other custom func
    add fundAccount function with an opportunity to choose a source: faucet or bank account or any other custom func

--- loadtesting/k6/tests/utils/accountManager.js
+++ loadtesting/k6/tests/utils/accountManager.js
@@ -1,9 +1,9 @@
 import wallet from 'k6/x/ethereum/wallet';
 import http from 'k6/http';
 import { check } from 'k6';
-
-const faucetUrl = __ENV.FAUCET_URL;
-const amount = 100;
+import { faucetUrl } from './consts.js';
+import { sendTokens } from './ethClient.js';
+import { waiter } from './helpers.js';
 
 export function createAccount() {
     var accountKey = wallet.generateKey();
@@ -13,7 +13,24 @@ export function createAccount() {
     };
 }
 
-export function fundAccountWithNeon(address, amount) {
+export function fundAccountFromFaucet(client, address, amount) {
+    fundAccount(client, requestFaucet, [address, amount], address, amount);
+}
+
+export function fundAccountFromBank(client, bankAccount, address, amount) {
+    fundAccount(client, sendTokens, [client, bankAccount, address, amount, null, null, null], address, amount);
+}
+
+async function fundAccount(client, fundFunc, fundFuncArgs, address, amount) {
+    let initialBalance = parseInt(client.getBalance(address));
+    if (initialBalance != 0) {
+        initialBalance = initialBalance / 10 ** 18;
+    }
+    await fundFunc(...fundFuncArgs);
+    waiter({ func: client.getBalance, args: [address], value: (initialBalance + amount) * 10 ** 18, retries: 60 });
+}
+
+function requestFaucet(address, amount) {
     const payload = JSON.stringify({
         amount: amount,
         wallet: address,
--- loadtesting/k6/tests/utils/consts.js
+++ loadtesting/k6/tests/utils/consts.js
@@ -1,3 +1,10 @@
+import http from 'k6/http';
+
+// params from envs
+let faucetUri = __ENV.FAUCET_URL;
+let faucetUrlObject;
+if (!faucetUri.includes("request_neon")) {
+    faucetUrlObject = http.url([faucetUri, 'request_neon']);
+}
+export const faucetUrl = faucetUrlObject;
 export const proxyUrl = __ENV.PROXY_URL;
-export const faucetUrl = __ENV.FAUCET_URL;
-export const networkId = parseInt(__ENV.NETWORK_ID);
