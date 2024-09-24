32dd7f3d2a3f4acda2323852fa99cdb6670bd45b
romanova-natasha <47331640+romanova-natasha@users.noreply.github.com>
2024-09-24 01:42:38 +0100

    add sender private key to client
    add sender private key to client

--- loadtesting/k6/tests/utils/ethClient.js
+++ loadtesting/k6/tests/utils/ethClient.js
@@ -2,16 +2,17 @@ import eth from 'k6/x/ethereum';
 import { proxyUrl, networkId } from './consts.js';
 
 
-export function ethClient() {
+export function ethClient(privateKey) {
     const client = new eth.Client({
         url: proxyUrl,
         chainID: networkId,
+        privateKey: privateKey,
     });
     return client;
 }
 
 export function sendNeon(client, from, to, amount, gas, gasPrice, nonce) {
-    const value = amount * 10e18;
+    const value = amount;
     return sendTokens(client, from, to, value, gas, gasPrice, nonce)
 }
 
@@ -38,8 +39,15 @@ export function sendTokens(client, from, to, value, gas, gasPrice, nonce) {
         "chain_id": client.chainID,
     };
 
-    if (transaction["gas"] == 0) {
-        transaction["gas"] = client.estimateGas(transaction);
+    if (gas == null) {
+        transaction["gas"] = client.estimateGas({
+            "from": from,
+            "to": to,
+            "value": value,
+            "input": [],
+            "gas_price": gasPrice,
+            "gas": 0,
+        });
     }
 
     const txh = client.sendRawTransaction(transaction);
