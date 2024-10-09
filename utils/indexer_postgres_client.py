57fe4b1dae1f5decc2a671edad60fbcf433acb5c
Ivan Adamov <ivan.adamov@neonfoundation.io>
2024-10-08 18:29:11 -0700

    added script for operator daily balance report

---
+++ utils/indexer_postgres_client.py
@@ -0,0 +1,112 @@
+import logging
+
+import psycopg2
+from psycopg2.extras import DictCursor, DictRow
+from tqdm import tqdm
+
+from utils.logger import create_logger
+
+QUERY_LIMIT = 50000
+
+
+class IndexerPostgresClient:
+    def __init__(self, host: str, db: str, user: str, password: str, port: int, log_level=logging.INFO):
+        self.logger = create_logger(__name__, level=log_level)
+        self.conn = psycopg2.connect(
+            database=db,
+            user=user,
+            password=password,
+            host=host,
+            port=port,
+        )
+
+    def get_latest_block_slot(self, finalized: bool) -> int:
+        query = """
+            SELECT block_slot
+            FROM solana_blocks
+            WHERE is_finalized = %s
+            AND is_active = TRUE
+            ORDER BY block_time DESC
+            LIMIT 1
+        """
+        with self.conn.cursor(name="fetch_latest_block_slot", cursor_factory=DictCursor) as cursor:
+            self.logger.info(f"Fetch latest block_slot")
+            cursor.execute(query, (finalized,))
+            block_slot = cursor.fetchall()[0]["block_slot"]
+        return block_slot
+
+    def get_solana_block_time(self, block: int) -> int:
+        query = """
+            SELECT block_time
+            FROM solana_blocks
+            WHERE block_slot = %s
+        """
+        with self.conn.cursor(name="fetch_solana_block_time", cursor_factory=DictCursor) as cursor:
+            self.logger.info(f"Fetch block {block} timestamp")
+            cursor.execute(query, (block,))
+            block_time = cursor.fetchall()[0]["block_time"]
+        return block_time
+
+    def get_operator_gas_data(
+        self,
+        keys: list[str],
+        from_slot: int,
+        to_slot: int,
+        operator_name: str,
+    ) -> list[DictRow]:
+        select_query = """
+            SELECT DISTINCT 
+                c.operator, 
+                c.sol_sig, 
+                b.block_slot, 
+                b.block_time, 
+                c.sol_spent, 
+                n.gas_price, 
+                s.neon_gas_used,
+                s.idx,
+                s.inner_idx
+            FROM 
+                solana_transaction_costs c
+            INNER JOIN 
+                neon_transactions n 
+                ON n.sol_sig = c.sol_sig
+            INNER JOIN 
+                solana_neon_transactions s 
+                ON s.neon_sig = n.neon_sig
+            INNER JOIN 
+                solana_blocks b 
+                ON c.block_slot = b.block_slot
+            WHERE 
+                c.operator = ANY(%s)
+                AND c.block_slot BETWEEN %s AND %s
+                AND b.is_finalized = TRUE
+                AND b.is_active = TRUE
+        """
+
+        count_query = f"""
+            SELECT COUNT(*)
+            FROM (
+                {select_query}
+            ) AS distinct_rows;
+        """
+
+        with self.conn.cursor() as cursor:
+            self.logger.info(f"Fetch operator {operator_name} transaction count")
+            cursor.execute(count_query, (keys, from_slot, to_slot))
+            total_rows = cursor.fetchone()[0]
+
+        rows = []
+
+        with self.conn.cursor(name="fetch_solana_sigs", cursor_factory=DictCursor) as cursor:
+            cursor.execute(select_query, (keys, from_slot, to_slot))
+
+            with tqdm(total=total_rows, desc=f"Fetching Operator {operator_name} gas data") as pbar:
+                while True:
+                    batch = cursor.fetchmany(QUERY_LIMIT)
+                    rows.extend(batch)
+                    pbar.update(len(batch))
+
+                    if len(batch) < QUERY_LIMIT:
+                        break
+
+        return rows
