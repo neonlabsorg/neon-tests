b6e872eafb3d9a763f3cff85fa486dbdccb4b064
Kristina Nikolaeva <kn@neonlabs.org>
2024-10-17 17:38:17 +0200

    fixed test_response_time

--- integration/tests/indexers_comparison/test_response_time.py
+++ integration/tests/indexers_comparison/test_response_time.py
@@ -5,8 +5,9 @@ from utils.helpers import wait_condition
 
 REPEAT_COUNT = 10
 
-def test_erc20_simple_trx(erc20_spl_mintable, endpoints, accounts, web3_client):
-    def sent_erc20_simple_trx():
+
+def test_erc20_simple_trx(erc20_spl_mintable, endpoints, accounts):
+    def sent_erc20_simple_trx(web3_client):
         signer = accounts[0]
         address_to = web3_client.create_account().address
         amount = 1
@@ -14,20 +15,19 @@ def test_erc20_simple_trx(erc20_spl_mintable, endpoints, accounts, web3_client):
         instruction_tx = erc20_spl_mintable.contract.functions.transfer(address_to, amount).build_transaction(tx)
         signed_tx = web3_client._web3.eth.account.sign_transaction(instruction_tx, signer.key)
         return web3_client._web3.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
-
-    trx_hash = sent_erc20_simple_trx()
+    trx_hash = sent_erc20_simple_trx(endpoints[1]["web3_client"])
     print(trx_hash)
     response_time_ba = get_time_to_get_receipt(endpoints[1], trx_hash)
     print(f"BA response time: {response_time_ba}")
 
-    trx_hash = sent_erc20_simple_trx()
+    trx_hash = sent_erc20_simple_trx(endpoints[0]["web3_client"])
     response_time_neon = get_time_to_get_receipt(endpoints[0], trx_hash)
     print(f"Neon response time: {response_time_neon}")
     assert response_time_ba <= response_time_neon
 
 
-def test_iterative_trx_contract_deploy(endpoints, web3_client, accounts):
-    def sent_iterative_trx_contract_deploy():
+def test_iterative_trx_contract_deploy(endpoints, accounts):
+    def sent_iterative_trx_contract_deploy(web3_client):
         signer = accounts[0]
         contract_interface = helpers.get_contract_interface(
             "EIPs/ERC3475",
@@ -40,23 +40,23 @@ def test_iterative_trx_contract_deploy(endpoints, web3_client, accounts):
         signed_tx = web3_client._web3.eth.account.sign_transaction(transaction, signer.key)
         return web3_client._web3.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
 
-    trx_hash = sent_iterative_trx_contract_deploy()
+    trx_hash = sent_iterative_trx_contract_deploy(endpoints[1]["web3_client"])
     print(trx_hash)
     response_time_ba = get_time_to_get_receipt(endpoints[1], trx_hash)
     print(f"BA response time: {response_time_ba}")
 
-    trx_hash = sent_iterative_trx_contract_deploy()
+    trx_hash = sent_iterative_trx_contract_deploy(endpoints[0]["web3_client"])
     response_time_neon = get_time_to_get_receipt(endpoints[0], trx_hash)
     print(f"Neon response time: {response_time_neon}")
     assert response_time_ba <= response_time_neon
 
 
-def test_big_iterative_trx(endpoints, web3_client, accounts):
-    contract, _ = web3_client.deploy_and_get_contract(
+def test_big_iterative_trx(endpoints, accounts):
+    contract, _ = endpoints[0]["web3_client"].deploy_and_get_contract(
         contract="common/Common", version="0.8.12", contract_name="MappingActions", account=accounts[1]
     )
 
-    def sent_big_iterative_trx():
+    def sent_big_iterative_trx(web3_client):
         signer = accounts[0]
         value = 25
         tx = web3_client.make_raw_tx(signer.address)
@@ -64,12 +64,12 @@ def test_big_iterative_trx(endpoints, web3_client, accounts):
         signed_tx = web3_client.eth.account.sign_transaction(instruction_tx, signer.key)
         return web3_client.eth.send_raw_transaction(signed_tx.rawTransaction).hex()
 
-    trx_hash = sent_big_iterative_trx()
+    trx_hash = sent_big_iterative_trx(endpoints[1]["web3_client"])
     print(trx_hash)
     response_time_ba = get_time_to_get_receipt(endpoints[1], trx_hash)
     print(f"BA response time: {response_time_ba}")
 
-    trx_hash = sent_big_iterative_trx()
+    trx_hash = sent_big_iterative_trx(endpoints[0]["web3_client"])
 
     response_time_neon = get_time_to_get_receipt(endpoints[0], trx_hash)
     print(f"Neon response time: {response_time_neon}")
@@ -78,7 +78,7 @@ def test_big_iterative_trx(endpoints, web3_client, accounts):
 
 def get_time_to_get_receipt(endpoint, trx_hash):
     time_before = time.time()
-    wait_condition(lambda: endpoint["client"].send_rpc("eth_getTransactionReceipt",
+    wait_condition(lambda: endpoint["rpc_client"].send_rpc("eth_getTransactionReceipt",
                                                        params=trx_hash)["result"] is not None, delay=0.1)
     time_after = time.time()
     return time_after - time_before
