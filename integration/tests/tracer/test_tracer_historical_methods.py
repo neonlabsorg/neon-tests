0a1b933e1e8b82fd6059fc17285a87775ffc412c
s-medvedev <40623263+s-medvedev@users.noreply.github.com>
2024-10-22 14:35:51 +0700

    Fix regex for NeonEVM version match (#446)

--- integration/tests/tracer/test_tracer_historical_methods.py
+++ integration/tests/tracer/test_tracer_historical_methods.py
@@ -347,7 +347,7 @@ class TestTracerHistoricalMethods:
         block = self.web3_client.get_block_number()
         revision = self.tracer_api.send_rpc(method="get_neon_revision", params=block)
         assert revision["result"]["neon_revision"] is not None
-        assert re.match(r"^[a-fA-F\d]{40}$", revision["result"]["neon_revision"])
+        assert re.match(r"^v[\d]+\.[\d]+\.[\d]+$|^[a-fA-F\d]{40}$", revision["result"]["neon_revision"])
 
     @pytest.mark.parametrize("block", [-190, '{"slot": 3f08}', "oneonetwozero", ["900"]])
     def test_neon_revision_invalid_block(self, block):
