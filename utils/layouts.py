23d3d86e37fcb628814a695b119a80e73b80409b
Kristina Nikolaeva <kn@neonlabs.org>
2024-10-24 18:35:30 +0200

    Revert "Temporary fix of emulate for neon_evm tests according to EmulateRequest changes."
    This reverts commit 9b598c415cac60b882cb23fa497144f10d2b1bb9.

--- utils/layouts.py
+++ utils/layouts.py
@@ -8,11 +8,10 @@ HOLDER_ACCOUNT_INFO_LAYOUT = Struct(
     "hash" / Bytes(32),
     "len" / Int64ul,
     # Technicall, heap_offset is not a part of Holder's Header and is located at a fixed
-    # memory location after the Header (with some padding, possibly).
+    # memory location after the Header with some padding.
     # But, since heap_offset is located strictly before the Buffer, we can
     # treat at as a part of the Header.
-    # P.S. Currently, the padding is zero bytes, uncomment and adjust in case it changes.
-    # "_padding" / Bytes(24),
+    "_padding" / Bytes(24),
     "heap_offset" / Int64ul,
 )
 
