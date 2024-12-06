581e8ec0019333a8d17cc196cdca49c0f319b086
Kristina Nikolaeva <kn@neonlabs.org>
2024-12-06 15:51:41 +0100

    added balance operator acc creating for sol chain

--- integration/tests/neon_evm/solana_native/test_scheduled_transactions.py
+++ integration/tests/neon_evm/solana_native/test_scheduled_transactions.py
@@ -18,17 +18,11 @@ from utils.scheduled_trx import ScheduledTransaction
 
 @pytest.mark.skipif(
     os.getenv("PROXY_URL") is not None,
-    reason="Skipped because the environment has a proxy, which causes the test to fail"
+    reason="Skipped because the environment has a proxy, which causes the test to fail",
 )
 class TestScheduledTrx:
     def test_execute_scheduled_trx_from_account(
-        self,
-        evm_loader,
-        neon_user: NeonUser,
-        treasury_pool,
-        basic_contract,
-        neon_api_client,
-        operator_keypair
+        self, evm_loader, neon_user: NeonUser, treasury_pool, basic_contract, neon_api_client, operator_keypair
     ):
         holder_acc = create_holder(operator_keypair, evm_loader)
         nonce = evm_loader.get_neon_nonce(neon_user.neon_address, SOL_CHAIN_ID)
@@ -151,9 +145,7 @@ class TestScheduledTrx:
         with pytest.raises(solana.rpc.core.RPCException, match=InstructionAsserts.TRANSACTION_TREE_NO_FEE):
             evm_loader.create_tree_account(neon_user, treasury_pool, tx.encode(), SOL_MINT_ID)
 
-    def test_out_of_gas(
-        self, evm_loader, neon_user: NeonUser, treasury_pool, operator_keypair, sender_with_wsol
-    ):
+    def test_out_of_gas(self, evm_loader, neon_user: NeonUser, treasury_pool, operator_keypair, sender_with_wsol):
         contract = deploy_contract(
             operator_keypair, sender_with_wsol, "transfers", evm_loader, treasury_pool, chain_id=SOL_CHAIN_ID
         )
@@ -168,19 +160,26 @@ class TestScheduledTrx:
             target=contract.eth_address,
             value=amount,
             call_data=data,
-            gas_limit=20000
+            gas_limit=20000,
         )
 
         with pytest.raises(solana.rpc.core.RPCException, match="transaction requires at least 25'000 gas limit"):
             evm_loader.create_tree_account(neon_user, treasury_pool, tx.encode(), SOL_MINT_ID)
 
     def test_send_sol_with_priority_fee(
-        self, evm_loader, neon_user: NeonUser, treasury_pool, neon_api_client, operator_keypair, sender_with_wsol
+        self,
+        evm_loader,
+        neon_user: NeonUser,
+        treasury_pool_new,
+        neon_api_client,
+        second_operator_keypair,
+        sender_with_wsol,
     ):
         contract = deploy_contract(
-            operator_keypair, sender_with_wsol, "transfers", evm_loader, treasury_pool, chain_id=SOL_CHAIN_ID
+            second_operator_keypair, sender_with_wsol, "transfers", evm_loader, treasury_pool_new,
+            chain_id=SOL_CHAIN_ID
         )
-        holder_acc = create_holder(operator_keypair, evm_loader)
+        holder_acc = create_holder(second_operator_keypair, evm_loader)
         nonce = evm_loader.get_neon_nonce(neon_user.neon_address, SOL_CHAIN_ID)
         data = abi.function_signature_to_4byte_selector("donate1000()")
         amount = 10000
@@ -193,14 +192,14 @@ class TestScheduledTrx:
             value=amount,
             call_data=data,
         )
-        operator_balance_before = evm_loader.get_operator_neon_balance(operator_keypair, SOL_CHAIN_ID)
+        operator_balance_before = evm_loader.get_operator_neon_balance(second_operator_keypair, SOL_CHAIN_ID)
         user_balance_before = evm_loader.get_neon_balance(neon_user.neon_address, SOL_CHAIN_ID)
-        treasury_balance_before = evm_loader.get_solana_balance(treasury_pool.account)
+        treasury_balance_before = evm_loader.get_solana_balance(treasury_pool_new.account)
 
-        tree_account = evm_loader.create_tree_account(neon_user, treasury_pool, tx.encode(), SOL_MINT_ID)
+        tree_account = evm_loader.create_tree_account(neon_user, treasury_pool_new, tx.encode(), SOL_MINT_ID)
 
         user_balance_after = evm_loader.get_neon_balance(neon_user.neon_address, SOL_CHAIN_ID)
-        treasury_balance_after = evm_loader.get_solana_balance(treasury_pool.account)
+        treasury_balance_after = evm_loader.get_solana_balance(treasury_pool_new.account)
         tree_account_balance = evm_loader.get_solana_balance(tree_account)
         user_balance_diff = user_balance_before - user_balance_after
         treasury_balance_diff = treasury_balance_before - treasury_balance_after
@@ -214,15 +213,21 @@ class TestScheduledTrx:
         )
         additional_accounts = [Pubkey.from_string(item["pubkey"]) for item in emulate_result["solana_accounts"]]
 
-        evm_loader.write_transaction_to_holder_account(tx.encode(), holder_acc, operator_keypair)
+        evm_loader.write_transaction_to_holder_account(tx.encode(), holder_acc, second_operator_keypair)
         evm_loader.execute_scheduled_trx_from_account(
-            0, operator_keypair, holder_acc, tree_account, treasury_pool, additional_accounts, compute_unit_price=3929
+            0,
+            second_operator_keypair,
+            holder_acc,
+            tree_account,
+            treasury_pool_new,
+            additional_accounts,
+            compute_unit_price=3929,
         )
-        evm_loader.finish_scheduled_trx(operator_keypair, tree_account, holder_acc)
+        evm_loader.finish_scheduled_trx(second_operator_keypair, tree_account, holder_acc)
         user_balance_before_destroy = evm_loader.get_neon_balance(neon_user.neon_address, SOL_CHAIN_ID)
 
-        evm_loader.destroy_tree_account(operator_keypair, neon_user, treasury_pool, tree_account)
+        evm_loader.destroy_tree_account(second_operator_keypair, neon_user, treasury_pool_new, tree_account)
         user_balance_after_destroy = evm_loader.get_neon_balance(neon_user.neon_address, SOL_CHAIN_ID)
         assert user_balance_after_destroy > user_balance_before_destroy
-        operator_balance_after = evm_loader.get_operator_neon_balance(operator_keypair, SOL_CHAIN_ID)
+        operator_balance_after = evm_loader.get_operator_neon_balance(second_operator_keypair, SOL_CHAIN_ID)
         assert operator_balance_after > operator_balance_before
