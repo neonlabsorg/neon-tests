0d12311e711a71ffcab01bde1e81664854d53975
QA get-together <testbyebeda@gmail.com>
2025-01-08 14:52:42 +0400

    add chrome argument

--- ui/tests/website_tests/conftest.py
+++ ui/tests/website_tests/conftest.py
@@ -26,6 +26,8 @@ def driver(request):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--disable-dev-tools")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-dev-tools")
 
     chrome_options.binary_location = "/usr/bin/google-chrome"
     driver = webdriver.Chrome()
