11db1fe0087c770b8db769401e657aa9835fd214
romanova-natasha <zabelina.nattaly@gmail.com>
2025-01-16 04:25:57 +0000

    rm useless case
    rm useless case

--- integration/tests/basic/evm/test_solana_interoperability.py
+++ integration/tests/basic/evm/test_solana_interoperability.py
@@ -120,29 +120,6 @@ class TestSolanaInteroperability:
         assert int.from_bytes(event_logs[0].args.value, byteorder="little") == next(get_counter_value)
         assert bytes32_to_solana_pubkey(event_logs[0].args.program.hex()) == COUNTER_ID
 
-    def test_iterative_actions_and_multiple_solana_calls_instructions_limit(self, counter_resource_address, call_solana_caller):
-        solana_calls = 15
-        iterations = 35
-        sender = self.accounts[0]
-        lamports = 0
-
-        instruction = Instruction(
-            program_id=COUNTER_ID,
-            accounts=[
-                AccountMeta(Pubkey(counter_resource_address), is_signer=False, is_writable=True),
-            ],
-            data=bytes([0x1]),
-        )
-        serialized = serialize_instruction(COUNTER_ID, instruction)
-
-        tx = self.web3_client.make_raw_tx(sender.address)
-        instruction_tx = call_solana_caller.functions.executeMultipleInIterativeMode(solana_calls,
-                                                                                     iterations,
-                                                                                     lamports,
-                                                                                     serialized).build_transaction(tx)
-        resp = self.web3_client.send_transaction(sender, instruction_tx)
-        assert resp["status"] == 0
-
     def test_transfer_with_pda_signature_iterative_tx_eip_1559(self, call_solana_caller, sol_client, solana_account):
         iterations = 20
         sender = self.accounts[0]
