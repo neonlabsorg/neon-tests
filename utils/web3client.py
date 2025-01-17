4455e7a31846c5502cba8658fe91886f47d33180
Ivan Adamov <ivan.adamov@neonfoundation.io>
2025-01-17 13:40:02 -0800

    updated eip-1559 tests

--- utils/web3client.py
+++ utils/web3client.py
@@ -11,6 +11,7 @@ import web3
 import web3.types
 from eth_abi import abi
 from eth_typing import BlockIdentifier
+from web3.contract import Contract
 from solders.pubkey import Pubkey
 from web3.exceptions import TransactionNotFound
 
@@ -239,7 +240,7 @@ class Web3Client:
             if gas:
                 transaction["gas"] = gas
         else:
-            if gas_price is not None and gas is not None:
+            if gas_price is not None:
                 max_priority_fee_per_gas, max_fee_per_gas = self.gas_price_to_eip1559_params(gas_price=gas_price)
             else:
                 max_priority_fee_per_gas = max_fee_per_gas = "auto"
@@ -266,9 +267,9 @@ class Web3Client:
         gas_multiplier: tp.Optional[float] = None,  # fix for some event depends transactions
         timeout: int = 120,
     ) -> web3.types.TxReceipt:
-        instruction_tx = self._web3.eth.account.sign_transaction(transaction, account.key)
-        signature = self._web3.eth.send_raw_transaction(instruction_tx.rawTransaction)
-        return self._web3.eth.wait_for_transaction_receipt(signature, timeout=timeout)
+        signed_tx = self._web3.eth.account.sign_transaction(transaction, account.key)
+        transaction_hash = self._web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+        return self._web3.eth.wait_for_transaction_receipt(transaction_hash, timeout=timeout)
 
     @allure.step("Send the scheduled transaction")
     def send_scheduled_transaction(
@@ -308,6 +309,7 @@ class Web3Client:
         gas: tp.Union[int, tp.Literal["auto"], None],
         max_priority_fee_per_gas: tp.Union[int, tp.Literal["auto"], None],
         max_fee_per_gas: tp.Union[int, tp.Literal["auto"], None],
+        base_fee_per_gas: tp.Union[int, tp.Literal["auto"]] = "auto",
         base_fee_multiplier: float = 1.1,
     ) -> web3.types.TxParams:
         # Handle addresses
@@ -321,6 +323,7 @@ class Web3Client:
         kwargs = locals().copy()
         del kwargs["self"]
         del kwargs["base_fee_multiplier"]
+        del kwargs["base_fee_per_gas"]
 
         # Move parameters related to gas to the end as they should be handled last
         for arg_name in ("gas", "max_priority_fee_per_gas", "max_fee_per_gas"):
@@ -332,9 +335,7 @@ class Web3Client:
         params = {"type": TransactionType.EIP_1559}
 
         # Map parameters with 'auto' value to their corresponding values
-        base_fee_per_gas = 10
-
-        if max_priority_fee_per_gas == "auto" or max_fee_per_gas == "auto":
+        if base_fee_per_gas == "auto":
             base_fee_per_gas = self.base_fee_per_gas()
 
         auto_map = {
@@ -376,7 +377,7 @@ class Web3Client:
         gas: tp.Optional[int] = 0,
         value=0,
         tx_type: TransactionType = TransactionType.LEGACY,
-    ) -> tp.Tuple[tp.Any, web3.types.TxReceipt]:
+    ) -> tp.Tuple[Contract, web3.types.TxReceipt]:
         contract_interface = helpers.get_contract_interface(
             contract,
             version,
@@ -395,7 +396,7 @@ class Web3Client:
             tx_type=tx_type,
         )
 
-        contract = self.eth.contract(address=contract_deploy_tx["contractAddress"], abi=contract_interface["abi"])
+        contract = self._web3.eth.contract(address=contract_deploy_tx["contractAddress"], abi=contract_interface["abi"])
 
         return contract, contract_deploy_tx
 
@@ -579,9 +580,12 @@ class Web3Client:
         ).json()
         return int(resp["result"]["tokenPriceUsd"], 16) / 100000
 
-    def gas_price_to_eip1559_params(self, gas_price: int) -> tuple[int, int]:
-        base_fee_per_gas = self.base_fee_per_gas()
-
+    def gas_price_to_eip1559_params(
+        self,
+        gas_price: int,
+        base_fee_multiplier: float = 1.1,
+    ) -> tuple[int, int]:
+        base_fee_per_gas = int(self.base_fee_per_gas() * base_fee_multiplier)
         msg = f"gas_price {gas_price} is lower than the baseFeePerGas {base_fee_per_gas}"
         assert gas_price >= base_fee_per_gas, msg
 
