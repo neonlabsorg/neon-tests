272730916222c9ccf4251b77901b2c03151a62ba
Kristina Nikolaeva <kn@neonlabs.org>
2025-02-05 10:53:07 +0100

    added tests to check account list

--- integration/tests/basic/erc/test_ERC20SPL.py
+++ integration/tests/basic/erc/test_ERC20SPL.py
@@ -8,11 +8,12 @@ from solana.rpc.types import TokenAccountOpts, TxOpts
 from solana.transaction import Transaction
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
+from solders.signature import Signature
 from spl.token import instructions
 from spl.token.constants import TOKEN_PROGRAM_ID
 
 from utils import metaplex
-from utils.consts import ZERO_ADDRESS
+from utils.consts import ZERO_ADDRESS, METAPLEX_ADDRESS, SPL_TOKEN_ADDRESS, CALL_SOLANA_ADDRESS, SOLANA_NATIVE_ADDRESS
 from utils.erc20wrapper import ERC20Wrapper
 from utils.helpers import gen_hash_of_block, wait_condition, create_invalid_address
 from utils.web3client import NeonChainWeb3Client
@@ -212,6 +213,20 @@ class TestERC20SPL:
         assert balance_acc1_after == balance_acc1_before - amount
         assert balance_acc2_after == balance_acc2_before + amount
         assert total_before == total_after
+
+    def test_transfer_and_check_sol_account_list_is_correct(self, erc20_contract, restore_balance, evm_loader):
+        new_account = self.accounts.create_account()
+
+        receipt = erc20_contract.transfer(erc20_contract.account, new_account.address, 100)
+        sol_trx = self.web3_client.get_solana_trx_by_neon(receipt["transactionHash"].hex())["result"][0]
+        solana_resp = evm_loader.get_transaction(Signature.from_string(sol_trx))
+        sol_accounts = solana_resp.value.transaction.transaction.message.account_keys
+        precompiled_addresses = [METAPLEX_ADDRESS, SPL_TOKEN_ADDRESS, CALL_SOLANA_ADDRESS, SOLANA_NATIVE_ADDRESS]
+        for precompiled_address in precompiled_addresses:
+            program_address = evm_loader.ether2program(precompiled_address[2:])[0]
+            assert (
+                Pubkey.from_string(program_address) not in sol_accounts
+            ), f"Program address for {precompiled_address} is in the account list"
 
     @pytest.mark.parametrize(
         "block_len, expected_exception, msg",
