853449e0d12c19e59f40256a3d4c432247d01f76
Iurii Baranik <bugaga1992@yahoo.com>
2025-02-12 10:31:14 +0200

    NDEV-3555 tests for neon_estimateScheduledGas

--- utils/web3client.py
+++ utils/web3client.py
@@ -616,7 +616,9 @@ class Web3Client:
         return resp["result"]
 
     @allure.step("Estimate list of scheduled transactions")
-    def estimate_scheduled(self, solana_payer: Pubkey, trx_list: tp.List[ScheduledTrxEstimateRequest]) -> dict:
+    def estimate_scheduled(
+        self, solana_payer: Pubkey, trx_list: tp.List[ScheduledTrxEstimateRequest], check_result: bool = True
+    ) -> dict:
         transactions = []
         for trx in trx_list:
             trx = {
@@ -637,8 +639,10 @@ class Web3Client:
             self._proxy_url,
             json=json,
         ).json()
-        assert "result" in resp, f"Failed to estimate transactions: {resp}"
-        return resp["result"]
+        if check_result:
+            assert "result" in resp, f"Failed to estimate transactions: {resp}"
+            return resp["result"]
+        return resp
 
 
 class NeonChainWeb3Client(Web3Client):
