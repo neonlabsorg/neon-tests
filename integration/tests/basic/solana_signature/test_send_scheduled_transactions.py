6c0602a986bbfdcfdcc52ab3de2ab27ce37e02cd
Iurii Baranik <bugaga1992@yahoo.com>
2025-02-20 17:24:02 +0200

    NDEV-3555 tests get_pending_transactions

--- integration/tests/basic/solana_signature/test_send_scheduled_transactions.py
+++ integration/tests/basic/solana_signature/test_send_scheduled_transactions.py
@@ -4,6 +4,7 @@ import allure
 import eth_abi
 import pytest
 from eth_utils import abi
+from solana.rpc.commitment import Confirmed
 
 from utils.consts import wSOL, LAMPORT_PER_SOL
 from utils.models.result import EthGetBlockByHashResult
@@ -291,7 +292,7 @@ class TestScheduledTrx:
         max_priority_fee_per_gas = 2500000000
         max_fee_per_gas = base_fee_per_gas * 2 + max_priority_fee_per_gas
 
-        tx0 = ScheduledTransaction(
+        tx = ScheduledTransaction(
             neon_user.neon_address,
             None,
             nonce,
@@ -306,9 +307,119 @@ class TestScheduledTrx:
         tree_acc_data = CreateTreeAccMultipleData(
             nonce=nonce, max_fee_per_gas=max_fee_per_gas, max_priority_fee_per_gas=max_priority_fee_per_gas
         )
-        tree_acc_data.add_trx(tx0, 0xFFFF, 0)
+        tree_acc_data.add_trx(tx, 0xFFFF, 0)
         evm_loader.create_tree_account_multiple(neon_user, treasury_pool, tree_acc_data.data, wSOL["address_spl"])
-        web3_client_sol.send_scheduled_transaction(tx0)
-        receipt = web3_client_sol.wait_for_transaction_receipt(tx0.hash())
+        web3_client_sol.send_scheduled_transaction(tx)
+        receipt = web3_client_sol.wait_for_transaction_receipt(tx.hash())
+        web3_client_sol.get_pending_transactions(neon_user.checksum_address)
         # for now, it is not possible to see error through the proxy
         assert receipt["status"] == 0
+
+    def test_neon_get_pending_scheduled_transaction_low_gas_price_status(
+        self, block_timestamp_contract, web3_client_sol, neon_user, treasury_pool, evm_loader, event_caller_contract
+    ):
+        contract, _ = block_timestamp_contract
+
+        nonce = web3_client_sol.get_nonce(neon_user.checksum_address)
+        call_data = abi.function_signature_to_4byte_selector("addDataToMapping(uint256,uint256)") + eth_abi.encode(
+            ["uint256", "uint256"], [1, 2]
+        )
+        gas_limit = 3000000
+        base_fee_per_gas = web3_client_sol.base_fee_per_gas()
+        max_priority_fee_per_gas = 2500000000
+        max_fee_per_gas = base_fee_per_gas * 2 + max_priority_fee_per_gas
+
+        tx = ScheduledTransaction(
+            neon_user.neon_address,
+            None,
+            nonce,
+            index=0,
+            target=contract.address,
+            call_data=call_data,
+            max_fee_per_gas=max_fee_per_gas,
+            max_priority_fee_per_gas=max_priority_fee_per_gas,
+            gas_limit=gas_limit,
+        )
+
+        tree_acc_data = CreateTreeAccMultipleData(
+            nonce=nonce, max_fee_per_gas=max_fee_per_gas, max_priority_fee_per_gas=max_priority_fee_per_gas
+        )
+        tree_acc_data.add_trx(tx, 0xFFFF, 0)
+        evm_loader.create_tree_account_multiple(neon_user, treasury_pool, tree_acc_data.data, wSOL["address_spl"])
+        web3_client_sol.send_scheduled_transaction(tx)
+        pending_trx = web3_client_sol.get_pending_transactions(neon_user.checksum_address)
+        assert pending_trx["0x0"][0]["status"] == "LowGasPrice"
+
+    def test_neon_get_pending_scheduled_transaction_no_tx_body(
+        self, web3_client_sol, neon_user, common_contract, evm_loader, treasury_pool
+    ):
+        contract_data = 18
+        data = abi.function_signature_to_4byte_selector("setNumber(uint256)") + eth_abi.encode(
+            ["uint256"], [contract_data]
+        )
+        trx_estimate_obj = ScheduledTrxEstimateRequest(neon_user.checksum_address, common_contract.address, data)
+        estimate_result = web3_client_sol.estimate_scheduled(neon_user.solana_account.pubkey(), [trx_estimate_obj])
+
+        tx = ScheduledTransaction.from_estimate_result(0, trx_estimate_obj, estimate_result)
+
+        evm_loader.create_tree_account(
+            neon_user, treasury_pool, tx.encode(), wSOL["address_spl"], chain_id=evm_loader.sol_chain_id
+        )
+        pending_trx = web3_client_sol.get_pending_transactions(neon_user.checksum_address)
+        assert pending_trx["0x0"][0]["status"] == "NoTransactionBody"
+
+    def test_neon_get_pending_scheduled_transaction_no_balance(
+        self, web3_client_sol, neon_user_no_sols, common_contract, evm_loader, treasury_pool, pytestconfig
+    ):
+
+        contract_data = 18
+        data = abi.function_signature_to_4byte_selector("setNumber(uint256)") + eth_abi.encode(
+            ["uint256"], [contract_data]
+        )
+        trx_estimate_obj = ScheduledTrxEstimateRequest(
+            neon_user_no_sols.checksum_address, common_contract.address, data
+        )
+        estimate_result = web3_client_sol.estimate_scheduled(
+            neon_user_no_sols.solana_account.pubkey(), [trx_estimate_obj]
+        )
+
+        tx = ScheduledTransaction.from_estimate_result(0, trx_estimate_obj, estimate_result)
+
+        evm_loader.create_tree_account(
+            neon_user_no_sols, treasury_pool, tx.encode(), wSOL["address_spl"], chain_id=evm_loader.sol_chain_id
+        )
+        pending_trx = web3_client_sol.get_pending_transactions(neon_user_no_sols.checksum_address)
+        assert pending_trx["0x0"][0]["status"] == "NoTransactionBody"
+
+    def test_send_simple_single_trx_a(
+        self, web3_client_sol, neon_user_no_sols, common_contract, evm_loader, treasury_pool, pytestconfig
+    ):
+        balance = evm_loader.get_solana_balance(neon_user_no_sols.solana_account.pubkey())
+        amount = 9214000
+        if pytestconfig.getoption("--network") != "mainnet":
+            if balance < amount * LAMPORT_PER_SOL:
+                evm_loader.request_airdrop(neon_user_no_sols.solana_account.pubkey(), amount, commitment=Confirmed)
+
+        contract_data = 18
+
+        data = abi.function_signature_to_4byte_selector("setNumber(uint256)") + eth_abi.encode(
+            ["uint256"], [contract_data]
+        )
+        trx_estimate_obj = ScheduledTrxEstimateRequest(
+            neon_user_no_sols.checksum_address, common_contract.address, data
+        )
+        estimate_result = web3_client_sol.estimate_scheduled(
+            neon_user_no_sols.solana_account.pubkey(), [trx_estimate_obj]
+        )
+
+        tx = ScheduledTransaction.from_estimate_result(0, trx_estimate_obj, estimate_result)
+
+        evm_loader.create_tree_account(
+            neon_user_no_sols, treasury_pool, tx.encode(), wSOL["address_spl"], chain_id=evm_loader.sol_chain_id
+        )
+        web3_client_sol.wait_for_transaction_receipt(tx.hash(), timeout=180)
+        pending_trx = web3_client_sol.get_pending_transactions(neon_user_no_sols.checksum_address)
+
+        assert len(pending_trx) >= 1
+        assert pending_trx[hex(tx.nonce)][0]["status"] in ("Done", "InProgress")
+        assert common_contract.functions.getNumber().call() == contract_data
