d5b651d818e87ad47a424e1b52f0ad6b613ce8f2
Sergei Bondar <mr.bondsv@gmail.com>
2025-02-25 18:27:01 +0400

    fix_gha

--- deploy/hetzner/main.tf
+++ deploy/hetzner/main.tf
@@ -46,22 +46,6 @@ resource "null_resource" "proxy_provision" {
 
   }
 
-
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "echo '${hcloud_server.solana.network.*.ip[0]}' > /tmp/solana_host",
-  #     "chmod a+x /tmp/proxy_init.sh",
-  #     "sudo /tmp/proxy_init.sh"
-  #   ]
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "root"
-  #     host        = hcloud_server.proxy.ipv4_address
-  #     private_key = file("/tmp/ci-stands")
-  #   }
-
-  # }
-
 }
 
 resource "hcloud_server" "solana" {
@@ -81,8 +65,6 @@ resource "hcloud_server" "solana" {
   network {
     network_id = data.hcloud_network.ci-network.id
   }
-
-  #user_data = data.template_file.solana_init.rendered
 
   labels = {
     environment = "ci"
