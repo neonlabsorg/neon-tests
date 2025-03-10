8ef725df5239daef2b98f738d881e381cc19047f
Kristina Nikolaeva <kn@neonlabs.org>
2025-03-10 10:28:26 +0100

    fix send_tokens

--- utils/web3client.py
+++ utils/web3client.py
@@ -494,7 +494,7 @@ class Web3Client:
                 max_priority_fee_per_gas=max_priority_fee_per_gas,
                 max_fee_per_gas=max_fee_per_gas,
             )
-        self.send_transaction(account=from_, transaction=transaction, timeout=180)
+        return self.send_transaction(account=from_, transaction=transaction, timeout=180)
 
     @allure.step("Send tokens under EIP-1559")
     def send_tokens_eip_1559(
