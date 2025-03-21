5447d5a0c896a1f8d23bf1d13dcb28ec2838b49f
Yury <46958252+ShamanEstBanan@users.noreply.github.com>
2025-03-21 15:17:55 +0200

    NDEV-3643 negative tests for preparatory_solana_trxs field

--- integration/tests/basic/rpc/test_rpc_neon_estimate_scheduled_gas.py
+++ integration/tests/basic/rpc/test_rpc_neon_estimate_scheduled_gas.py
@@ -521,6 +521,9 @@ class TestNeonRPCEstimateScheduledGasChildTrx:
         assert is_hex(estimate_result["gasList"][0])
         assert is_hex(estimate_result["gasList"][1])
 
+
+class TestNeonRPCEstimateScheduledGasPreparatorySolTrx:
+
     def test_estimate_with_preparatory_solana_transactions(
         self, web3_client_sol, neon_user, erc20_spl_mintable_new, evm_loader, treasury_pool
     ):
@@ -607,3 +610,120 @@ class TestNeonRPCEstimateScheduledGasChildTrx:
             neon_user.solana_account.pubkey(), [trx_estimate_obj1, trx_estimate_obj2], check_result=False
         )
         assert "execution reverted: External call fails" in resp["error"]["message"], "Error message is not correct"
+
+    def test_user_has_no_sols(
+        self, web3_client_sol, neon_user_no_sols, erc20_spl_mintable_new, evm_loader, treasury_pool
+    ):
+        recipient = NeonUser(evm_loader.loader_id)
+        ata_amount = 1_000
+        erc20_spl_mintable_new.approve(erc20_spl_mintable_new.account, neon_user_no_sols.checksum_address, ata_amount)
+
+        my_ata = get_associated_token_address(
+            neon_user_no_sols.solana_account.pubkey(), erc20_spl_mintable_new.token_mint_pubkey
+        )
+        solana_contract_account = Pubkey.from_string(
+            evm_loader.ether2program(erc20_spl_mintable_new.contract.address)[0]
+        )
+
+        trx = Transaction()
+        trx.add(
+            create_associated_token_account(
+                neon_user_no_sols.solana_account.pubkey(),
+                neon_user_no_sols.solana_account.pubkey(),
+                erc20_spl_mintable_new.token_mint_pubkey,
+            )
+        )
+        trx.add(
+            approve(
+                ApproveParams(
+                    program_id=TOKEN_PROGRAM_ID,
+                    source=my_ata,
+                    delegate=solana_contract_account,
+                    owner=neon_user_no_sols.solana_account.pubkey(),
+                    amount=ata_amount,
+                )
+            )
+        )
+
+        data1 = decode_function_signature(
+            "transferSolanaFrom(address,bytes32,uint64)",
+            [erc20_spl_mintable_new.account.address, bytes(my_ata), ata_amount],
+        )
+        data2 = decode_function_signature("transfer(address,uint256)", [recipient.checksum_address, ata_amount])
+
+        trx_estimate_obj1 = ScheduledTrxEstimateRequest(
+            neon_user_no_sols.checksum_address, erc20_spl_mintable_new.address, data1
+        )
+        trx_estimate_obj2 = ScheduledTrxEstimateRequest(
+            neon_user_no_sols.checksum_address, erc20_spl_mintable_new.address, data2
+        )
+
+        resp = web3_client_sol.estimate_scheduled(
+            neon_user_no_sols.solana_account.pubkey(),
+            [trx_estimate_obj1, trx_estimate_obj2],
+            preparatory_solana_trxs=trx.instructions,
+            check_result=False,
+        )
+        assert resp["error"]["code"] == Error3.CODE
+        assert "execution reverted: External call fails" in resp["error"]["message"], "Error message is not correct"
+
+    # TODO спросить Крис что за прикол с амаунтом, почему не падает ошибка что запросили больше одобренного
+    def test_error_in_preparatory_solana_transactions(
+        self, web3_client_sol, neon_user, erc20_spl_mintable_new, evm_loader, treasury_pool
+    ):
+        recipient = NeonUser(evm_loader.loader_id)
+        ata_amount = 10
+        erc20_spl_mintable_new.approve(erc20_spl_mintable_new.account, neon_user.checksum_address, ata_amount)
+
+        my_ata = get_associated_token_address(
+            neon_user.solana_account.pubkey(), erc20_spl_mintable_new.token_mint_pubkey
+        )
+        solana_contract_account = Pubkey.from_string(
+            evm_loader.ether2program(erc20_spl_mintable_new.contract.address)[0]
+        )
+
+        trx = Transaction()
+        trx.add(
+            create_associated_token_account(
+                neon_user.solana_account.pubkey(),
+                neon_user.solana_account.pubkey(),
+                erc20_spl_mintable_new.token_mint_pubkey,
+            )
+        )
+        ata_wrong_amount = 1_000_000
+        trx.add(
+            approve(
+                ApproveParams(
+                    program_id=TOKEN_PROGRAM_ID,
+                    source=my_ata,
+                    delegate=solana_contract_account,
+                    owner=neon_user.solana_account.pubkey(),
+                    amount=ata_wrong_amount,
+                )
+            )
+        )
+
+        data1 = decode_function_signature(
+            "transferSolanaFrom(address,bytes32,uint64)",
+            [erc20_spl_mintable_new.account.address, bytes(my_ata), ata_amount],
+        )
+        data2 = decode_function_signature("transfer(address,uint256)", [recipient.checksum_address, ata_amount])
+
+        trx_estimate_obj1 = ScheduledTrxEstimateRequest(
+            neon_user.checksum_address, erc20_spl_mintable_new.address, data1
+        )
+        trx_estimate_obj2 = ScheduledTrxEstimateRequest(
+            neon_user.checksum_address, erc20_spl_mintable_new.address, data2
+        )
+
+        resp = web3_client_sol.estimate_scheduled(
+            neon_user.solana_account.pubkey(),
+            [trx_estimate_obj1, trx_estimate_obj2],
+            preparatory_solana_trxs=trx.instructions,
+            check_result=False,
+        )
+        assert len(resp["gasList"]) == 2, "Amount of transactions must be 2"
+
+        nonce = web3_client_sol.get_nonce(neon_user.checksum_address)
+        assert resp["nonce"] == hex(nonce)
+        assert_fields_are_hex(resp, ["chainId", "maxFeePerGas", "maxPriorityFeePerGas", "nonce", "treasuryIndex"])
