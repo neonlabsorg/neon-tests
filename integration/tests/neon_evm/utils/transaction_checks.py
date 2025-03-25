7e42fafb06cc6f980c63b007f8510e9af51b8aaf
Yury <46958252+ShamanEstBanan@users.noreply.github.com>
2025-03-25 13:12:11 +0200

    NDEV-3498 delete unused function

--- integration/tests/neon_evm/utils/transaction_checks.py
+++ integration/tests/neon_evm/utils/transaction_checks.py
@@ -16,17 +16,6 @@ def check_transaction_logs_have_text(
     assert text in logs, f"Transaction logs don't contain '{text}'. Logs: {logs}"
 
 
-def check_transaction_logs_not_have_text(
-    solana_client: SolanaClient, trx: GetTransactionResp | SendTransactionResp, text: str
-) -> None:
-    if isinstance(trx, GetTransactionResp):
-        receipt = trx
-    else:
-        receipt = solana_client.get_transaction(trx, commitment=Confirmed)
-    logs = decode_logs(receipt.value.transaction.meta.log_messages)
-    assert text not in logs, f"Transaction logs don't contain '{text}'. Logs: {logs}"
-
-
 def check_holder_account_tag(solana_client: SolanaClient, storage_account, layout, expected_tag):
     account_data = solana_client.get_account_info(storage_account, commitment=Confirmed).value.data
     parsed_data = layout.parse(account_data)
