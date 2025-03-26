123154a2fb7bbb523bd3c224f16da6d9eafcd754
Yury <46958252+ShamanEstBanan@users.noreply.github.com>
2025-03-26 15:00:48 +0200

    NDEV-3643 fixed review issues

--- integration/tests/basic/rpc/test_rpc_neon_estimate_scheduled_gas.py
+++ integration/tests/basic/rpc/test_rpc_neon_estimate_scheduled_gas.py
@@ -555,26 +555,24 @@ class TestNeonRPCEstimateScheduledGas:
                 "data": trx.data,
                 "value": trx.value,
             }
-            if trx.child_transaction:
-                transaction["childTransaction"] = trx.child_transaction
             transactions.append(transaction)
         params = {"scheduledSolanaPayer": str(solana_payer), "transactions": transactions}
-        if preparatory_solana_trxs:
-            instructions = []
-            for trx in preparatory_solana_trxs:
-                instruction = {"programId": str(trx.program_id), "data": base58.b58encode(trx.data).decode("utf-8")}
-                accounts = []
-                for account in trx.accounts:
-                    accounts.append(
-                        {
-                            "address": str(account.pubkey),
-                            "isWritable": account.is_writable,
-                            "isSigner": account.is_signer,
-                        }
-                    )
-                instruction["accounts"] = accounts
-                instructions.append(instruction)
-            params["preparatorySolanaTransactions"] = [{"instructions": instructions}]
+
+        instructions = []
+        for trx in preparatory_solana_trxs:
+            instruction = {"programId": str(trx.program_id), "data": base58.b58encode(trx.data).decode("utf-8")}
+            accounts = []
+            for account in trx.accounts:
+                accounts.append(
+                    {
+                        "address": str(account.pubkey),
+                        "isWritable": account.is_writable,
+                        "isSigner": account.is_signer,
+                    }
+                )
+            instruction["accounts"] = accounts
+            instructions.append(instruction)
+        params["preparatorySolanaTransactions"] = [{"instructions": instructions}]
 
         if case == "wrong_data":
             params["preparatorySolanaTransactions"][0]["instructions"][1]["data"] = "-"
@@ -588,3 +586,65 @@ class TestNeonRPCEstimateScheduledGas:
         assert resp["error"]["code"] == Error32602.CODE
         assert resp["error"]["message"] == Error32602.INVALID_PARAMETERS
         assert "Value error" in resp["error"]["data"]["errors"][0]
+
+    def test_estimate_with_preparatory_reverted_solana_transactions(
+        self, web3_client_sol, neon_user, erc20_spl_mintable_new, evm_loader
+    ):
+        recipient = NeonUser(evm_loader.loader_id)
+        ata_amount = 0
+        wrong_ammount = 1000000000
+        erc20_spl_mintable_new.approve(erc20_spl_mintable_new.account, neon_user.checksum_address, ata_amount)
+
+        my_ata = get_associated_token_address(
+            neon_user.solana_account.pubkey(), erc20_spl_mintable_new.token_mint_pubkey
+        )
+        solana_contract_account = Pubkey.from_string(
+            evm_loader.ether2program(erc20_spl_mintable_new.contract.address)[0]
+        )
+
+        trx = Transaction()
+        trx.add(
+            create_associated_token_account(
+                neon_user.solana_account.pubkey(),
+                neon_user.solana_account.pubkey(),
+                erc20_spl_mintable_new.token_mint_pubkey,
+            )
+        )
+        trx.add(
+            approve(
+                ApproveParams(
+                    program_id=TOKEN_PROGRAM_ID,
+                    source=my_ata,
+                    delegate=solana_contract_account,
+                    owner=neon_user.solana_account.pubkey(),
+                    amount=ata_amount,
+                )
+            )
+        )
+
+        data1 = decode_function_signature(
+            "transferSolanaFrom(address,bytes32,uint64)",
+            [erc20_spl_mintable_new.account.address, bytes(my_ata), wrong_ammount],
+        )
+        data2 = decode_function_signature("transfer(address,uint256)", [recipient.checksum_address, ata_amount])
+
+        trx_estimate_obj1 = ScheduledTrxEstimateRequest(
+            neon_user.checksum_address, erc20_spl_mintable_new.address, data1
+        )
+        trx_estimate_obj2 = ScheduledTrxEstimateRequest(
+            neon_user.checksum_address, erc20_spl_mintable_new.address, data2
+        )
+
+        resp = web3_client_sol.estimate_scheduled(
+            neon_user.solana_account.pubkey(),
+            [trx_estimate_obj1, trx_estimate_obj2],
+            preparatory_solana_trxs=trx.instructions,
+            check_result=False,
+        )
+        assert resp["error"]["code"] == Error3.CODE
+        assert resp["error"]["message"] == Error3.EXECUTION_REVERTED
+        assert len(resp["gasList"]) == 2, "Amount of transactions must be 2"
+
+        nonce = web3_client_sol.get_nonce(neon_user.checksum_address)
+        assert resp["nonce"] == hex(nonce)
+        assert_fields_are_hex(resp, ["chainId", "maxFeePerGas", "maxPriorityFeePerGas", "nonce", "treasuryIndex"])
