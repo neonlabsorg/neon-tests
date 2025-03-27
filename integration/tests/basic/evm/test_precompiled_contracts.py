fb5d7009d76dc69d663242806c98dfb1c69cd267
IvanAdamovNeon <169074337+IvanAdamovNeon@users.noreply.github.com>
2025-03-27 12:46:13 -0700

    Return sol to bank (#586)
    * add case with dependent txs
    
    add case with dependent txs
    
    * add case with pda and ata used in scheduled txs
    
    add case with pda and ata used in scheduled txs
    
    * add send tokens to random user from prepared ones, fix naming
    
    add send tokens to random user from prepared ones, fix naming
    
    * enable test for scheduled tx tracing
    
    enable test for scheduled tx tracing
    
    * Rerun added
    
    * Install rerun
    
    * Changed test run
    
    * Rerun install
    
    * Update ui.yml
    
    change run position
    
    * Update clickfile.py
    
    args added
    
    * added rerun into ui.yml
    
    * run changed
    
    * flaky added
    
    * Tf actions fix branch (#570)
    
    * fix treasury_pool (#572)
    
    * check neon users balances during the load tests
    
    check neon users balances during the load tests
    
    * added logs for test_call_via_send_trx (#577)
    
    * NDEV-3498 simple test, check accounts list and logs (#576)
    
    * NDEV-3498 simple test, check accounts list and logs
    
    * fixed test_call_via_send_trx
    
    * update clickfile remove flaky
    
    * Update ui.txt
    
    * update clickfile
    
    * website_tests --reruns 3 --reruns-delay 2
    
    * moved back retry
    
    * Ndev 3604 compare speed (#578)
    
    1. Added indexer speed comparison
    2. Added shared resources implementation to erc20spl.p
    3. Added user value link to "prepare_contract" setup
    
    * pytest version changed
    
    * added tags trigger for CI (#579)
    
    * return pytest 8
    
    * improve retries
    
    * added assertions for subscribtion
    
    * Added separated devnet workflow (#581)
    
    * Ndev 3649 separated devnet run (#582)
    
    * Ndev 3643 tests for childTransaction and preparatory_solana_trxs
    
    * NDEV-3643 tests for childTransaction &  negative tests for preparatory_solana_trxs field
    
    * moved cost report related code from clickfile.py (#573)
    
    * moved cost report related code from clickfile.py
    
    * fixed branch
    
    * revert!
    
    * fixed circular import
    
    * fix devnet run (#583)
    
    * return SOL to bank account (#571)
    
    ---------
    
    Co-authored-by: romanova-natasha <zabelina.nattaly@gmail.com>
    Co-authored-by: romanova-natasha <47331640+romanova-natasha@users.noreply.github.com>
    Co-authored-by: QA get-together <testbyebeda@gmail.com>
    Co-authored-by: mrbnd <66267303+mrbnd@users.noreply.github.com>
    Co-authored-by: kseniia_taktasheva <37874880+e-b-e-d-a@users.noreply.github.com>
    Co-authored-by: Kristina Nikolaeva <112946046+kristinaNikolaevaa@users.noreply.github.com>
    Co-authored-by: Yury <46958252+ShamanEstBanan@users.noreply.github.com>
    Co-authored-by: Kristina Nikolaeva <kn@neonlabs.org>
    Co-authored-by: Stan Shiganov <stanislav.shiganov@gmail.com>

--- integration/tests/basic/evm/test_precompiled_contracts.py
+++ integration/tests/basic/evm/test_precompiled_contracts.py
@@ -5,6 +5,7 @@ import random
 
 import pytest
 
+from integration.tests.basic.helpers.rpc_checks import check_trx_is_success
 from utils.accounts import EthAccounts
 from utils.web3client import NeonChainWeb3Client
 
@@ -142,16 +143,9 @@ class TestPrecompiledContracts:
         instruction_tx = self.web3_client.make_raw_tx(
             sender_account, address, data=input_data, amount=amount, estimate_gas=True
         )
-        if request.node.callspec.id not in [
-            "modexp-nagydani-5-square0",
-            "modexp-nagydani-5-square1",
-            "modexp-nagydani-5-qube0",
-            "modexp-nagydani-5-qube1",
-            "modexp-nagydani-5-pow0x100010",
-            "modexp-nagydani-5-pow0x100011",
-        ]:
+        if "modexp-nagydani-5" not in request.node.callspec.id:
             receipt = self.web3_client.send_transaction(sender_account, instruction_tx)
-            assert receipt["status"] == 1
+            check_trx_is_success(self.web3_client, evm_loader, receipt["transactionHash"].hex())
 
             if pytestconfig.getoption("--network") not in ["devnet", "night-stand"]:
                 assert self.web3_client.get_balance(address) - balance_before == amount
@@ -163,8 +157,7 @@ class TestPrecompiledContracts:
             except ValueError as exc:
                 assert "InvalidLength" in exc.args[0]["message"]
 
-    @pytest.mark.xdist_group("precompiled_contract_balance")
-    def test_send_neon_without_data(self, pytestconfig):
+    def test_send_neon_without_data(self, pytestconfig, evm_loader):
         address = "0x0000000000000000000000000000000000000006"
         sender_account = self.accounts[0]
         balance_before = self.web3_client.get_balance(address)
@@ -172,7 +165,7 @@ class TestPrecompiledContracts:
         instruction_tx = self.web3_client.make_raw_tx(sender_account.address, address, amount=amount, estimate_gas=True)
         receipt = self.web3_client.send_transaction(sender_account, instruction_tx)
 
-        assert receipt["status"] == 1
+        check_trx_is_success(self.web3_client, evm_loader, receipt["transactionHash"].hex())
         pytestconfig.getoption("--network")
         if pytestconfig.getoption("--network") not in ["devnet", "night-stand"]:
             assert self.web3_client.get_balance(address) - balance_before == amount
