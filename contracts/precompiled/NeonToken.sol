5877ee235025a89cb5936045c712573bac116766
Mikhail Surin <surinmike@gmail.com>
2025-04-05 22:27:20 +0300

    fix test_withdraw test

--- contracts/precompiled/NeonToken.sol
+++ contracts/precompiled/NeonToken.sol
@@ -3,13 +3,25 @@ pragma solidity >=0.7.6;
 
 interface INeonWithdraw {
     function withdraw(bytes32) external payable returns (bool);
+
+    function withdraw_on_chain(uint64 chain_id, bytes32 to, uint256 amount) external returns (bool);
 }
 
 contract NeonToken {
     INeonWithdraw constant NeonPrecompiled =
         INeonWithdraw(0xFF00000000000000000000000000000000000003);
 
-    function withdraw(bytes32 spender) external payable {
-        NeonPrecompiled.withdraw{value: msg.value}(spender);
+    uint64 chain_id;
+
+    constructor() {
+        uint bigChainId;
+        assembly {
+            bigChainId := chainid()
+        }
+        chain_id = uint64(bigChainId);
+    }
+
+    function withdraw(bytes32 solana_address) external payable {
+        NeonPrecompiled.withdraw_on_chain(chain_id, solana_address, msg.value);
     }
 }
