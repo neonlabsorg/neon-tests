a9c04dd1b01674ebd47b40c88f630e293bbbfc9c
Yury <46958252+ShamanEstBanan@users.noreply.github.com>
2025-05-07 10:10:35 +0300

    fix const wSOL for linters

--- integration/tests/basic/solana_signature/test_send_scheduled_transactions_erc20.py
+++ integration/tests/basic/solana_signature/test_send_scheduled_transactions_erc20.py
@@ -2,6 +2,7 @@ import allure
 import pytest
 from solana.rpc.commitment import Confirmed
 from solders.pubkey import Pubkey
+from spl.token.constants import WRAPPED_SOL_MINT
 from spl.token.instructions import get_associated_token_address
 
 from integration.tests.basic.helpers.rpc_checks import check_trx_is_success
@@ -313,7 +314,7 @@ class TestScheduledTrxERC20:
         tree_acc_data.add_trx(trxs[1], 2, 0)
         tree_acc_data.add_trx(trxs[2], 0xFFFF, 2)
 
-        evm_loader.create_tree_account_multiple(neon_user, treasury_pool, tree_acc_data.data, wSOL["address_spl"])
+        evm_loader.create_tree_account_multiple(neon_user, treasury_pool, tree_acc_data.data, WRAPPED_SOL_MINT)
         web3_client_sol.send_all_scheduled_transactions(trxs)
 
         for trx in trxs:
