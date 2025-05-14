1decc6a09235eaf9b109ea3001a9f71e48e0dc00
Yury <46958252+ShamanEstBanan@users.noreply.github.com>
2025-05-14 13:11:37 +0200

    NDEV 3705 review fixes

--- contracts/common/ExpectedErrorsChecker.sol
+++ contracts/common/ExpectedErrorsChecker.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
 contract A {
     int a = 0;
     uint public M;
@@ -17,6 +18,35 @@ contract A {
         for (uint i = 0; i < 5; i++) {
             text_array[i] = text;
         }
+    }
+
+    function cancel_in_iterative_tx(uint N, address resaver) public payable {
+        for (uint i = 1; i <= N; i++) {
+            if (i == N / 2) {
+                method1();
+            }
+        }
+    }
+
+    function loopWithMethodCall(uint N, address payable recipient) public payable {
+        for (uint i = 1; i <= N; i++) {
+            if (i == N / 2) {
+                // Отправка всего полученного value получателю
+                (bool success,) = recipient.call{value: msg.value}("");
+                require(success, "Transfer failed");
+
+                method1();
+            }
+        }
+    }
+
+    function loopAndReturnNumber(uint N) public pure returns (uint) {
+        uint result;
+
+        for (uint i = 0; i < N; i++) {
+            result = i;
+        }
+        return result;
     }
 
     function riskyDivision(uint x, uint y) public pure returns (uint) {
