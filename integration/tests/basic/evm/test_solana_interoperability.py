c3c7fc213431be8290ebdbd277869356968d70b6
Kristina Nikolaeva <kn@neonlabs.org>
2025-05-21 18:25:39 +0200

    fix test_solana_call_after_iterative_actions_exceed_accounts_limit

--- integration/tests/basic/evm/test_solana_interoperability.py
+++ integration/tests/basic/evm/test_solana_interoperability.py
@@ -525,7 +525,7 @@ class TestSolanaInteroperability:
         tx = self.web3_client.make_raw_tx(sender.address)
 
         with pytest.raises(
-            web3.exceptions.ContractLogicError,
+            web3.exceptions.Web3RPCError,
             match="too many accounts",
         ):
             call_solana_caller.functions.executeInIterativeMode(loop_count, lamports, serialized).build_transaction(tx)
