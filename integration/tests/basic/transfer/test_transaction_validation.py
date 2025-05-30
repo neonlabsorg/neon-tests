ccf819a022a288dc09909f8ada6a59dd970f462e
Kristina Nikolaeva <kn@neonlabs.org>
2025-05-30 12:29:43 +0200

    removed test_send_transaction_with_small_gas_price

--- integration/tests/basic/transfer/test_transaction_validation.py
+++ integration/tests/basic/transfer/test_transaction_validation.py
@@ -3,11 +3,9 @@ import re
 
 import allure
 import pytest
-import web3
 from web3.exceptions import Web3RPCError
 
 from integration.tests.basic.helpers.assert_message import ErrorMessage
-from integration.tests.basic.helpers.rpc_checks import is_hex
 from utils.accounts import EthAccounts
 from utils.web3client import NeonChainWeb3Client
 from utils.helpers import gen_hash_of_block
@@ -83,20 +81,6 @@ class TestTransactionsValidation:
         response = json_rpc_client.send_rpc("eth_sendRawTransaction", params)
         assert ErrorMessage.TOO_BIG_TRANSACTION.value in response["error"]["message"]
         assert response["error"]["code"] == -32000
-
-    def test_send_transaction_with_small_gas_price(self, json_rpc_client):
-        """Check that transaction can't be accepted if gas value is too small"""
-        gas_price = self.web3_client.gas_price()
-        sender_account = self.accounts[8]
-        recipient_account = self.accounts[1]
-        transaction = self.web3_client.make_raw_tx(
-            from_=sender_account, to=recipient_account, amount=1, gas_price=(int(gas_price * 0.01)), estimate_gas=True
-        )
-        signed_tx = self.web3_client.eth.account.sign_transaction(transaction, sender_account.key)
-        response = json_rpc_client.send_rpc("eth_sendRawTransaction", [signed_tx.raw_transaction.hex()])
-        assert is_hex(response["result"])
-        with pytest.raises(web3.exceptions.TimeExhausted, match="is not in the chain after 10 seconds"):
-            self.web3_client._web3.eth.wait_for_transaction_receipt(response["result"], timeout=10)
 
     def test_big_memory_value(self):
         sender_account = self.accounts[0]
