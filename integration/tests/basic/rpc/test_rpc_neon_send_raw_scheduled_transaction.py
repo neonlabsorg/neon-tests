57ea8ef59efc99f7562113da17eb8ae41c30c8f5
Yury <46958252+ShamanEstBanan@users.noreply.github.com>
2025-07-03 13:28:42 +0200

    NDEV-3609

--- integration/tests/basic/rpc/test_rpc_neon_send_raw_scheduled_transaction.py
+++ integration/tests/basic/rpc/test_rpc_neon_send_raw_scheduled_transaction.py
@@ -33,7 +33,6 @@ class TestNeonRPCSendRAWTransaction:
             resp["error"]["data"]["errors"][0] == "Method neon_sendRawScheduledTransaction expect 1 parameters, got 2."
         )
 
-    @pytest.mark.xfail(reason="NDEV-3609")
     def test_repeat_call_with_same_trx_hash(
         self, web3_client_sol, neon_user, common_contract, evm_loader, treasury_pool
     ):
@@ -46,15 +45,8 @@ class TestNeonRPCSendRAWTransaction:
         evm_loader.create_tree_account(neon_user, treasury_pool, tx.encode())
 
         web3_client_sol.send_scheduled_transaction(tx, check_result=False)
-        resp_for_second_sent_no_waiting = web3_client_sol.send_scheduled_transaction(tx, check_result=False)
-        assert "error" in resp_for_second_sent_no_waiting, "must be error for second sending the same transaction"
-
-        web3_client_sol.wait_for_transaction_receipt(tx.hash(), timeout=180)  # wait until first tx finished
-        resp_after_waiting = web3_client_sol.send_scheduled_transaction(tx, check_result=False)
-
-        assert "error" in resp_after_waiting
-        assert Error32000.CODE == resp_after_waiting["error"]["code"]
-        assert Error32000.UNKNOWN_TRANSACTION_HASH == resp_after_waiting["error"]["message"]
+        resp = web3_client_sol.send_scheduled_transaction(tx, check_result=False)
+        assert "result" in resp
 
     def test_no_tree_account_for_trx(self, web3_client_sol, neon_user, common_contract, evm_loader, treasury_pool):
         data = decode_function_signature("setNumber(uint256)", [18])
