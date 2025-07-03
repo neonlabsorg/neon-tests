01dea16c2f400fac0b1f10d45a1780391b594858
Ivan Adamov <ivan.adamov@neonfoundation.io>
2025-07-03 12:58:58 -0700

    add more operators

--- integration/tests/neon_evm/test_compute_units.py
+++ integration/tests/neon_evm/test_compute_units.py
@@ -693,7 +693,7 @@ class TestComputeUnits:
                     done = True
                     break
                 if "ExitError" in log:
-                    raise AssertionError(f"EVM Returned error in logs: {receipt}")
+                    raise AssertionError(f"EVM Returned an error in logs: {receipt}")
 
             allure_attach_accounts_data(resp=receipt, evm_loader=evm_loader, title=f"Used accounts data {i}")
 
