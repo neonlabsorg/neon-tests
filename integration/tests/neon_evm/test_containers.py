44939be9f454fc327dfb1df7df014797741278e2
Kristina Nikolaeva <kn@neonlabs.org>
2025-07-18 17:22:32 +0200

    work version of test

--- integration/tests/neon_evm/test_containers.py
+++ integration/tests/neon_evm/test_containers.py
@@ -1,4 +1,5 @@
 from integration.tests.neon_evm.utils.ethereum import make_contract_call_trx
+from integration.tests.neon_evm.utils.transaction_checks import check_transaction_logs_have_text
 
 
 def test_assemble_container(
@@ -9,14 +10,13 @@ def test_assemble_container(
         sender=sender_with_tokens.eth_address.hex(),
         contract=rw_lock_contract.eth_address.hex(),
         function_signature=function_signature,
-        params=[3],
+        params=[10],
     )
-    signed_tx = make_contract_call_trx(evm_loader, sender_with_tokens, rw_lock_contract, function_signature, [3])
+    signed_tx = make_contract_call_trx(evm_loader, sender_with_tokens, rw_lock_contract, function_signature, [10])
     evm_loader.execute_transaction_steps_from_instruction(
         operator_keypair, treasury_pool, holder_acc, signed_tx, emulate_accounts
     )
     data_accounts = evm_loader.get_data_accounts(emulate_accounts)
-    print("data_accounts", data_accounts)
 
     evm_loader.assemble_container(
         operator=operator_keypair,
@@ -27,19 +27,19 @@ def test_assemble_container(
 
     accounts_for_execution_with_container = [
         rw_lock_contract.solana_address,
-        rw_lock_contract.balance_account_address,
         sender_with_tokens.solana_account_address,
         sender_with_tokens.balance_account_address,
     ]
 
-    signed_tx = make_contract_call_trx(evm_loader, sender_with_tokens, rw_lock_contract, function_signature, [3])
-    evm_loader.execute_transaction_steps_from_instruction(
+    signed_tx = make_contract_call_trx(evm_loader, sender_with_tokens, rw_lock_contract, function_signature, [10])
+    resp = evm_loader.execute_transaction_steps_from_instruction(
         operator_keypair,
         treasury_pool,
         holder_acc,
         signed_tx,
         accounts_for_execution_with_container,
     )
+    check_transaction_logs_have_text(solana_client=evm_loader, trx=resp, text="exit_status=0x11")
 
 
 #
