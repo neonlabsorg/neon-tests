7d08b7bb577608ac4f340c3fd844e4b99113b66c
Kristina Nikolaeva <kn@neonlabs.org>
2025-07-18 17:43:39 +0200

    added allocate_container in the test

--- integration/tests/neon_evm/test_containers.py
+++ integration/tests/neon_evm/test_containers.py
@@ -17,7 +17,7 @@ def test_assemble_container(
         operator_keypair, treasury_pool, holder_acc, signed_tx, emulate_accounts
     )
     data_accounts = evm_loader.get_data_accounts(emulate_accounts)
-
+    # assemble container and execute transaction with it
     evm_loader.assemble_container(
         operator=operator_keypair,
         treasury=treasury_pool,
@@ -41,26 +41,17 @@ def test_assemble_container(
     )
     check_transaction_logs_have_text(solana_client=evm_loader, trx=resp, text="exit_status=0x11")
 
+    # allocate container and execute transaction with it
+    evm_loader.allocate_container(
+        operator=operator_keypair, treasury=treasury_pool, container_address=rw_lock_contract.solana_address, size=32
+    )
 
-#
-# def test_allocate_container(
-#     evm_loader, operator_keypair, treasury_pool, sender_with_tokens, neon_api_client, rw_lock_contract, holder_acc
-# ):
-#     function_signature = "update_storage(uint256)"
-#     contract_accounts = neon_api_client.get_additional_accounts_by_emulation(
-#         sender=sender_with_tokens.eth_address.hex(),
-#         contract=rw_lock_contract.eth_address.hex(),
-#         function_signature=function_signature,
-#         params=[5],
-#     )
-#     signed_tx = make_contract_call_trx(evm_loader, sender_with_tokens, rw_lock_contract, function_signature, [5])
-#     evm_loader.execute_transaction_steps_from_instruction(
-#         operator_keypair, treasury_pool, holder_acc, signed_tx, contract_accounts
-#     )
-#
-#     evm_loader.allocate_container(
-#         operator=operator_keypair,
-#         treasury=treasury_pool,
-#         container_address=rw_lock_contract.solana_address,
-#         accounts=contract_accounts,
-#     )
+    signed_tx = make_contract_call_trx(evm_loader, sender_with_tokens, rw_lock_contract, function_signature, [10])
+    resp = evm_loader.execute_transaction_steps_from_instruction(
+        operator_keypair,
+        treasury_pool,
+        holder_acc,
+        signed_tx,
+        accounts_for_execution_with_container,
+    )
+    check_transaction_logs_have_text(solana_client=evm_loader, trx=resp, text="exit_status=0x11")
