2819c01907408fa9b452fe1aef160245865866f8
Kristina Nikolaeva <kn@neonlabs.org>
2025-07-29 18:23:40 +0200

    added test_erc20_for_spl_containerized

--- integration/tests/basic/containers/test_containers.py
+++ integration/tests/basic/containers/test_containers.py
@@ -136,3 +136,46 @@ class TestContainers:
             check_trx_is_success(web3_client_sol, evm_loader, trx.hash().hex(), timeout=120)
 
         wait_condition(lambda: not evm_loader.account_exists(tree_account), timeout_sec=120, delay=2)
+
+    def test_erc20_for_spl_containerized(self, accounts, evm_loader, multiple_actions_erc20, treasury_pool, operator):
+
+        acc_1, contract = multiple_actions_erc20
+        acc_2 = accounts[2]
+        mint_amount = 1000
+        transfer_amount_1 = 300
+        transfer_amount_2 = 200
+
+        container_address = Pubkey.from_string(evm_loader.ether2program(contract.address[2:])[0])
+        erc20_address = contract.functions.getErc20Address().call()
+        erc20_sol_address = Pubkey.from_string(evm_loader.ether2program(erc20_address[2:])[0])
+        print("multiple_actions_erc20 address:", container_address)
+        print("erc20 address:", erc20_sol_address)
+
+        tx = self.web3_client.make_raw_tx(acc_1)
+        instruction_tx = contract.functions.mintTransferTransfer(
+            mint_amount,
+            acc_1.address,
+            transfer_amount_1,
+            acc_2.address,
+            transfer_amount_2,
+        ).build_transaction(tx)
+
+        signed_tx = self.web3_client.eth.account.sign_transaction(instruction_tx, acc_1.key)
+        result = self.web3_client.get_neon_emulate(str(signed_tx.raw_transaction.hex()))
+        sol_accounts = [Pubkey.from_string(item["pubkey"]) for item in result["result"]["solanaAccounts"]]
+        data_accounts = evm_loader.filter_neon_accounts_by_type(sol_accounts, AccountType.STORAGE)
+        balance_acc = evm_loader.filter_neon_accounts_by_type(sol_accounts, AccountType.USER_BALANCE)
+        contract_accounts = evm_loader.filter_neon_accounts_by_type(sol_accounts, AccountType.CONTRACT)
+        print("data accounts:", len(data_accounts))
+        print("balance accounts:", len(balance_acc))
+        print("contract accounts:", len(contract_accounts))
+
+        evm_loader.assemble_container(
+            operator.operator_keypairs[0],
+            treasury_pool,
+            container_address,
+            [erc20_sol_address],
+        )
+
+        receipt = self.web3_client.send_transaction(acc_1, instruction_tx)
+        assert receipt["status"] == 1, "Transaction should be successful"
