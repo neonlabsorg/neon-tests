490321e6d86d440aff93b04d40978616f2849ced
Kristina Nikolaeva <kn@neonlabs.org>
2025-08-14 16:04:02 +0200

    Update Revert.sol

--- contracts/common/Revert.sol
+++ contracts/common/Revert.sol
@@ -72,10 +72,3 @@ contract Caller {
         new FailingContract(true);
     }
 }
-
-
-contract contractWithConstructorWithReturn {
-
-    constructor() returns (uint256) {
-        return 42;
-    }
